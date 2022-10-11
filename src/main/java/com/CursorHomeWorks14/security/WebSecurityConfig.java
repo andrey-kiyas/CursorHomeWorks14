8c05bf89fc4826123540207808629199e5ea1f8a
ANDREY <andrey.kiyas@gmail.com>
2022-10-11 23:57:16 +0300

    PR01 "Homework 14"

--- src/main/java/com/CursorHomeWorks14/security/WebSecurityConfig.java
+++ src/main/java/com/CursorHomeWorks14/security/WebSecurityConfig.java
@@ -20,7 +20,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
